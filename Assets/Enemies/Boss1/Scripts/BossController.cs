a3a9483de11d73f7fd4ed529bac5e9bb384ab5d8
Lucas Meres <93730460+Mero0GF@users.noreply.github.com>
2024-05-14 20:38:21 -0300

    tentando melhorar o boss

--- Assets/Enemies/Boss1/Scripts/BossController.cs
+++ Assets/Enemies/Boss1/Scripts/BossController.cs
@@ -10,6 +10,7 @@ public class BossController : MonoBehaviour
     {
         FollowingPlayer,
         SeekingHead,
+        MeleeAtk,
     }
     public State state;
 
@@ -19,6 +20,14 @@ public class BossController : MonoBehaviour
     private float speed;
     public Vector3 headPos = new Vector3(0, 3);
     public Vector3 rockSpawnPos = new Vector3(0, 10);
+    private float distance;
+    private Vector2 playerPos;
+
+    public float atkRange = 4;
+    private float atkSpd = 25;
+    private float atkCd = 0;
+    private float atkCdMax = 120;
+    private float atkCdMin = 0;
 
     [SerializeField] BossManager manager;
     public GameObject barrier;
@@ -41,6 +50,8 @@ public class BossController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
         switch (state) {
             case State.FollowingPlayer:
                 transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
@@ -56,12 +67,29 @@ public class BossController : MonoBehaviour
                     }
                     else barrier.SetActive(false);
                 }
+                if (distance <= atkRange && atkCd <= 0)
+                {
+                    playerPos = player.transform.position;
+                    state = State.MeleeAtk;
+                }
+                atkCd = Mathf.Clamp(atkCd-1,atkCdMin,atkCdMax);
+                //Debug.Log(atkCd);
                 break;
 
 
             case State.SeekingHead:
                 transform.position = Vector2.MoveTowards(transform.position, head.transform.position, speed * Time.deltaTime);
                 SpawnRock();
+                break;
+
+            case State.MeleeAtk:
+                head.transform.position = transform.position + headPos;
+                barrier.transform.position = transform.position + new Vector3(0, 0.5f);
+                SpawnRock();
+                if (atkCd <= 0)
+                {
+                    StartCoroutine(MeleeAtk());
+                }
                 break;
         }
     }
@@ -97,6 +125,33 @@ public class BossController : MonoBehaviour
         gotHit = false;
         head.transform.position = transform.position + headPos;
         bossHead.state = BossHead.State.Static;
+        state = State.FollowingPlayer;
+    }
+
+    IEnumerator MeleeAtk()
+    {
+        int i = 0;
+        atkCd = atkCdMax;
+        yield return new WaitForSeconds(1f);
+        playerPos = player.transform.position;
+        while (i < 10)
+        {
+            i++;
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, atkSpd * Time.deltaTime);
+        }
+        yield return new WaitForSeconds(1f);
+        playerPos = player.transform.position;
+        for (i = 0; i < 10; i++)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, atkSpd * Time.deltaTime);
+        }
+        yield return new WaitForSeconds(1.5f);
+        playerPos = player.transform.position;
+        for (i = 0; i < 10; i++)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, atkSpd* 3 * Time.deltaTime);
+        }
+        yield return new WaitForSeconds(2f);
         state = State.FollowingPlayer;
     }
 
